a6fd9e6bb9b16bd300b88d0b162358a774de0940
Klova Nick <nikolayklova404@gmail.com>
2022-01-05 12:50:28 +0200

    Adding the function of obtaining information about roles to the client.

---
+++ WADatabase/Administration/Managment/RoleManagment.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WADatabase.Administration.Clients;
+
+namespace WADatabase.Administration.Managment
+{
+    public class RoleManagment
+    {
+        public async Task<IEnumerable<Models.API.Response.ReturnRole>> GetAllAsync()
+        {
+            WorldAirlinesClient db = new WorldAirlinesClient();
+
+            List<Models.API.Response.ReturnRole> response = new List<Models.API.Response.ReturnRole>();
+
+            await using (db.context)
+            {
+                var role = db.context.Roles
+                    .ToListAsync()
+                    .Result;
+
+                if (role == null)
+                    throw new Exception("Not found");
+                else
+                {
+                    for (int i = 0; i < role.Count; i++)
+                    {
+                        Models.API.Response.ReturnRole item = new Models.API.Response.ReturnRole
+                        {
+                            Id = role[i].Id,
+                            Role = role[i].Role1
+                        };
+
+                        response.Add(item);
+                    }
+                }
+
+                return response;
+            }
+        }
+    }
+}
---
+++ WorldAirlineServer/Controllers/RoleController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WADatabase.Administration.Managment;
+
+namespace WorldAirlineServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/getRoles")]
+        public async Task<IActionResult> GetRoles()
+        {
+            try
+            {
+                RoleManagment role = new RoleManagment();
+                var response = await role.GetAllAsync();
+
+                return StatusCode(200, response);
+            }
+            catch (Exception e)
+            {
+                if (e.Message == "Not found")
+                    return StatusCode(404, "Not found");
+                else
+                    return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
