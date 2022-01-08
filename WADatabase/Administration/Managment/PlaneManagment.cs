67a56feb488c6e151368d73fde9860e7e251a346
Klova Nick <nikolayklova404@gmail.com>
2022-01-08 16:54:13 +0200

    The ability to manage planes directly from the API.

---
+++ WADatabase/Administration/Managment/PlaneManagment.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WADatabase.Administration.Clients;
+using WADatabase.Models.API.Request;
+
+namespace WADatabase.Administration.Managment
+{
+    public class PlaneManagment
+    {
+        public async Task<IEnumerable<Models.API.Response.ReturnPlane>> GetAllPlanes()
+        {
+            WorldAirlinesClient db = new WorldAirlinesClient();
+
+            await using (db.context)
+            {
+                var planes = db.context.Planes
+                    .ToListAsync()
+                    .Result;
+
+
+                List<Models.API.Response.ReturnPlane> response = new List<Models.API.Response.ReturnPlane>();
+
+                foreach (var plane in planes)
+                {
+                    Models.API.Response.ReturnPlane item = new Models.API.Response.ReturnPlane
+                    {
+                        Id = plane.Id,
+                        Number = plane.Number,
+                        Model = plane.Model,
+                        ManufactureDate = plane.ManufactureDate,
+                        LifeTime = plane.LifeTime,
+                        Ok = plane.Ok
+                    };
+
+                    response.Add(item);
+                }
+
+                return response;
+            }
+        }
+
+        public async Task<Models.API.Response.ReturnPlane> GetPlaneById(int id)
+        {
+            WorldAirlinesClient db = new WorldAirlinesClient();
+
+            await using (db.context)
+            {
+                var plane = db.context.Planes
+                    .ToListAsync()
+                    .Result
+                    .FirstOrDefault(x => x.Id == id);
+
+                Models.API.Response.ReturnPlane item = new Models.API.Response.ReturnPlane
+                {
+                    Id = plane.Id,
+                    Number = plane.Number,
+                    Model = plane.Model,
+                    ManufactureDate = plane.ManufactureDate,
+                    LifeTime = plane.LifeTime,
+                    Ok = plane.Ok
+                };
+
+                return item;
+            }
+        }
+
+        public async Task CreatePlane(ReceivedPlane incomingData)
+        {
+            WorldAirlinesClient db = new WorldAirlinesClient();
+
+            await using (db.context)
+            {
+                Models.DB_Request.Plane plane = new Models.DB_Request.Plane
+                {
+                    Model = incomingData.Model,
+                    Number = incomingData.Number,
+                    ManufactureDate = incomingData.ManufactureDate,
+                    Ok = incomingData.Ok,
+                    LifeTime = incomingData.LifeTime
+                };
+
+                db.context.Add(plane);
+                db.context.SaveChanges();
+            }
+        }
+
+        public async Task DeletePlane (int id)
+        {
+            WorldAirlinesClient db = new WorldAirlinesClient();
+
+            await using (db.context)
+            {
+                var plane = db.context.Planes
+                    .ToListAsync()
+                    .Result
+                    .FirstOrDefault(x => x.Id == id);
+
+                db.context.Remove(plane);
+                db.context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ WorldAirlineServer/Controllers/PlaneController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WADatabase.Administration.Managment;
+using WADatabase.Models.API.Request;
+
+namespace WorldAirlineServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaneController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/getAllPlanes")]
+        public async Task<IActionResult> GetAllPlanes()
+        {
+            try
+            {
+                PlaneManagment db = new PlaneManagment();
+
+                var result = await db.GetAllPlanes();
+
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("/getPlanesById")]
+        public async Task<IActionResult> GetPlanesById(int id)
+        {
+            try
+            {
+                PlaneManagment db = new PlaneManagment();
+
+                var result = await db.GetPlaneById(id);
+
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("/createPlane")]
+        public async Task<IActionResult> CreatePlane(ReceivedPlane incomingData)
+        {
+            try
+            {
+                PlaneManagment db = new PlaneManagment();
+
+                await db.CreatePlane(incomingData);
+
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete]
+        [Route("/deletePlane")]
+        public async Task<IActionResult> DeletePlane(int id)
+        {
+            try
+            {
+                PlaneManagment db = new PlaneManagment();
+
+                await db.DeletePlane(id);
+
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
